f46d28a243eacf4fe6e4465dd46240eba61feff1
Henry Cullen <119402282@umail.ucc.ie>
2022-11-11 10:52:12 +0000

    first commit

---
+++ .history/src/OffsetType_20221111104037.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function OffsetType() {
+  return (
+    <div className="offset">
+      
+    </div>
+  );
+}
+
+export default OffsetType();
