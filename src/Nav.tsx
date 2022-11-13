e7754b8d8cc0a68d98dcd559b85e58b6d3cbd8f4
119402282 <119402282@umail.ucc.ie>
2022-11-13 15:59:54 +0000

    added some bnasic styling and nav stuff

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -8,9 +8,13 @@ interface IProps {
 export const Nav :FC<IProps> = () => {
 
     return (
-        <nav>
-            <a href="#">Login</a>
-            <a href="#">Share</a>
-        </nav>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="#">Login</a></li>
+                    <li><a href="#">Share</a></li>
+                </ul>
+            </nav>
+        </header>
     );
 };
